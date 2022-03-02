62a459b2286699df55fc83dddb4654febcec7e2a
Vihar Vaghasiya <42845610+vihar73@users.noreply.github.com>
2022-03-02 14:42:31 +0530

    Add files via upload
    made vars in main tool local

--- BMI_Calc_tool.py
+++ BMI_Calc_tool.py
@@ -3,25 +3,24 @@ import process_json
 import logging
 import os
 
-
 logger = logging.getLogger(__name__)
-
-#input path for json file
-filein = os.path.join(os.getcwd(),'data','sample_data_input.json')
-
-#output path for json file
-fileout = os.path.join(os.getcwd(),'Sample','sample_data_output.json')
-
-#required columns in json input
-req_cols = {'Gender', 'HeightCm','WeightKg'}
-
-#BMI table for calculation
-bmi_list = [("Underweight",[-np.inf,18.5],"Malnutrition risk"),("Normal Weight",[18.5,25],"Low risk"),
-            ("Overweight",[25,30],"Enhanced risk"),("Moderately obese",[30,35],"Medium Risk"),
-            ("Severely obese",[35,40],"High risk"),("Very severely obese",[40,np.inf],"Very high risk")]
 
 #processing json and writing output
 if __name__ == "__main__":
+    #input path for json file
+    filein = os.path.join(os.getcwd(),'data','sample_data_input.json')
+
+    #output path for json file
+    fileout = os.path.join(os.getcwd(),'Sample','sample_data_output.json')
+
+    #required columns in json input
+    req_cols = {'Gender', 'HeightCm','WeightKg'}
+
+    #BMI table for calculation
+    bmi_list = [("Underweight",[-np.inf,18.5],"Malnutrition risk"),("Normal Weight",[18.5,25],"Low risk"),
+                ("Overweight",[25,30],"Enhanced risk"),("Moderately obese",[30,35],"Medium Risk"),
+                ("Severely obese",[35,40],"High risk"),("Very severely obese",[40,np.inf],"Very high risk")]
+
     try:
         process_json.main(filein, fileout, req_cols, bmi_list)
         
